722b5f1b0c15bf65826118038a2595315ce0908b
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-10-09 08:21:03 +0000

    update

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -3,20 +3,28 @@
 0. Pascal's Triangle
 Mock Technical Interview
 """
-
-
 def pascal_triangle(n):
+    """Returns a list of lists of integers representing Pascal’s triangle of n"""
+    
+    
+    triangle = []
+    
+    
     if n <= 0:
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        row = [1]
-        for j in range(1, i):
-            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1)
+        return triangle
+    
+    
+    for i in range(n):
+        
+        row = []
+        C = 1
+        
+        
+        for j in range(i + 1):
+            row.append(C)
+            C = C * (i - j) // (j + 1)
+        
+        
         triangle.append(row)
-
+    
     return triangle
-
