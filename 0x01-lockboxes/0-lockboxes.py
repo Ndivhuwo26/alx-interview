b6e64bf1bb1f430e3b7adc6467682fe1b2e7ae96
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-10-24 07:58:01 +0000

    lockbox

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-
+"""
+Check if all boxes can be opened
+"""
 def canUnlockAll(boxes):
-    """Check if all boxes can be opened."""
-    if len(boxes) == 0:
+    if not boxes:
         return False
 
-    """ A set to track which boxes have been opened"""
-    opened = {0}
-    keys = boxes[0]
+    opened = set([0])
+    keys = list(boxes[0])
 
-    """ Iterate over the available keys"""
-    for key in keys:
+    while keys:
+        key = keys.pop(0)
         if key < len(boxes) and key not in opened:
             opened.add(key)
             keys.extend(boxes[key])
 
     return len(opened) == len(boxes)
+
