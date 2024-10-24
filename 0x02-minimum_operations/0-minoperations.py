c05a04407d1944af9f900a327ae6b2eb02af659b
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-10-24 07:33:14 +0000

    inoperations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+"""
+The minOperations(n) function calculates the fewest number of operations needed to create exactly n characters in a text file using only two operations
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operation = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operation += divisor
+            n //= divisor
+        divisor += 1
+
+    return operation
+
