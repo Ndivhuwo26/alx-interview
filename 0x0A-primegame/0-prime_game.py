1f0f72c11b585e95426c6298697574aef823a2bd
Ndivhuwo26 <mogakwekevin@gmail.com>
2024-12-12 08:29:44 +0000

    prime

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -7,40 +7,41 @@ n
 """
 
 def isWinner(x, nums):
-    if x < 1 or not nums:
+    """x - rounds
+    nums - numbers list
+    """
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
         return None
 
-    # Determine the maximum number to compute primes up to
-    max_n = max(nums)
+    ben = 0
+    maria = 0
 
-    # Generate a list of primes up to max_n using Sieve of Eratosthenes
-    is_prime = [True] * (max_n + 1)
-    is_prime[0] = is_prime[1] = False
-    for i in range(2, int(max_n**0.5) + 1):
-        if is_prime[i]:
-            for j in range(i * i, max_n + 1, i):
-                is_prime[j] = False
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
 
-    # Precompute the cumulative count of primes up to each number
-    prime_count = [0] * (max_n + 1)
-    for i in range(1, max_n + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
-
-    maria_wins = 0
-    ben_wins = 0
-
-    # Simulate each round
-    for n in nums:
-        if prime_count[n] % 2 == 0:
-            ben_wins += 1
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
         else:
-            maria_wins += 1
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+            maria += 1
+    if ben > maria:
         return "Ben"
-    else:
-        return None
+    if maria > ben:
+        return "Maria"
+    return None
+
+
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
 
